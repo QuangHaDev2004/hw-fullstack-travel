7395abab415eae14604b1d7c7cbd631d67aa3736
QuangHaDev2004 <ngoquangha2004@gmail.com>
2025-08-15 08:28:44 +0700

    Tích hợp pug

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Trang chu')
-})
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.get('/tours', (req, res) => {
-  res.send('Danh sach tour')
-})
+app.get("/", (req, res) => {
+  res.render("client/pages/home", {
+    pageTitle: "Trang chu",
+  });
+});
+
+app.get("/tours", (req, res) => {
+  res.render("client/pages/tour-list", {
+    pageTitle: "Danh sach tour",
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
