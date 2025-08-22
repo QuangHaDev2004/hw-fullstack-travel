0ea9559f260c660a66ba0f9af464b4e7e3bc8b79
QuangHaDev2004 <ngoquangha2004@gmail.com>
2025-08-22 15:43:36 +0700

    Tích hợp giao diện fe admin

---
+++ controllers/admin/account.controller.js
@@ -0,0 +1,29 @@
+module.exports.login = (req, res) => {
+  res.render("admin/pages/login", {
+    pageTitle: "Đăng nhập",
+  });
+};
+
+module.exports.register = (req, res) => {
+  res.render("admin/pages/register", {
+    pageTitle: "Đăng ký",
+  });
+};
+
+module.exports.forgotPassword = (req, res) => {
+  res.render("admin/pages/forgot-password", {
+    pageTitle: "Quên mật khẩu",
+  });
+};
+
+module.exports.otpPassword = (req, res) => {
+  res.render("admin/pages/otp-password", {
+    pageTitle: "Nhập mã OTP",
+  });
+};
+
+module.exports.resetPassword = (req, res) => {
+  res.render("admin/pages/reset-password", {
+    pageTitle: "Đổi mật khẩu",
+  });
+};
