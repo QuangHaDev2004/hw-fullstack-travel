e99e176bf8cb7cef67955eecf2542957540fa34d
QuangHaDev2004 <ngoquangha2004@gmail.com>
2025-09-08 22:23:24 +0700

    validate be

--- routes/admin/category.route.js
+++ routes/admin/category.route.js
@@ -1,13 +1,21 @@
 const router = require("express").Router();
 const categoryController = require("../../controllers/admin/category.controller");
+
 const multer = require("multer");
 const cloudinaryHelper = require("../../helpers/cloudinary.helper");
 const upload = multer({ storage: cloudinaryHelper.storage });
+
+const categoryValidate = require("../../validates/admin/category.validate");
 
 router.get("/list", categoryController.list);
 
 router.get("/create", categoryController.create);
 
-router.post("/create", upload.single("avatar"), categoryController.createPost);
+router.post(
+  "/create",
+  upload.single("avatar"),
+  categoryValidate.createPost,
+  categoryController.createPost
+);
 
 module.exports = router;
