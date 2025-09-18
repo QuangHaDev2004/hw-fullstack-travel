d6fa28b0865c780fd21321b3b1f17743a4f05385
QuangHaDev2004 <ngoquangha2004@gmail.com>
2025-09-18 20:13:07 +0700

    profile edit change password feature

--- controllers/admin/profile.controller.js
+++ controllers/admin/profile.controller.js
@@ -1,6 +1,8 @@
 const AccountAdmin = require("../../models/account-admin.model");
 const Role = require("../../models/role.model");
 
+const bcrypt = require("bcryptjs");
+
 module.exports.edit = async (req, res) => {
   console.log(req.account);
   if (req.account.role) {
@@ -56,5 +58,23 @@ module.exports.editPatch = async (req, res) => {
 module.exports.changePassword = (req, res) => {
   res.render("admin/pages/profile-change-password", {
     pageTitle: "Đổi mật khẩu",
+  });
+};
+
+module.exports.changePasswordPatch = async (req, res) => {
+  const salt = await bcrypt.genSalt(10);
+  req.body.password = await bcrypt.hash(req.body.password, salt);
+
+  await AccountAdmin.updateOne(
+    {
+      _id: req.account.id,
+      deleted: false,
+    },
+    req.body
+  );
+
+  res.json({
+    code: "success",
+    message: "Đổi mật khẩu thành công!",
   });
 };
