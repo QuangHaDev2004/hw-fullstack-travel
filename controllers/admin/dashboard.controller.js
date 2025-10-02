413ec66e734ff62773212174b6e9b7e9b4a1daa9
QuangHaDev2004 <ngoquangha2004@gmail.com>
2025-10-02 16:52:58 +0700

    feat: box overview

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,5 +1,32 @@
-module.exports.dashboard = (req, res) => {
+const AccountAdmin = require("../../models/account-admin.model");
+const Order = require("../../models/order.model");
+
+module.exports.dashboard = async (req, res) => {
+  // Thông số tổng quan
+  const overview = {
+    totalAdmin: 0,
+    totalOrder: 0,
+    totalRevenue: 0,
+  };
+
+  overview.totalAdmin = await AccountAdmin.countDocuments({
+    deleted: false,
+  });
+
+  const orderList = await Order.find({
+    deleted: false,
+  });
+
+  overview.totalOrder = orderList.length;
+  overview.totalRevenue = orderList.reduce(
+    (total, item) => total + item.total,
+    0
+  );
+
+  // Đơn hàng mới
+
   res.render("admin/pages/index", {
     pageTitle: "Tổng quan",
+    overview: overview,
   });
 };
